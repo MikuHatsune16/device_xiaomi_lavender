5733a95034b8b361f67fd340fc74a3d459347c40
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    sweet: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf

--- device.mk
+++ device.mk
@@ -380,6 +380,10 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc \
     fstab.qcom
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_lavender
