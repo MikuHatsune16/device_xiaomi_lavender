893a4b6ae6a9a90f4f9cd94b894777dd77ee5ceb
MikuHatsune16 <106465182+MikuHatsune16@users.noreply.github.com>
2022-12-27 19:35:02 +0530

    lavender: Use nexus clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,8 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lavender-perf_defconfig
 TARGET_KERNEL_VERSION := 4.4
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := nexus-clang
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/nexus-clang
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
